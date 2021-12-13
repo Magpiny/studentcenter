be6c18092c5560cdd08e127b620b558e6bf63fb0
Magpiny <magpinyb@gmail.com>
2021-12-13 03:57:28 +0300

    production commits

--- studentcenter/settings.py
+++ studentcenter/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+# import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -157,7 +157,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-prod_db = dj_database_url.config(conn_max_age=None)
-DATABASES['default'].update(prod_db)
+# prod_db = dj_database_url.config(conn_max_age=None)
+# DATABASES['default'].update(prod_db)
 
 django_heroku.settings(locals())
