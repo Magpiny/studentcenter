832dfd692c7ca90f4777a7f330b5eb99f6ff3a69
Magpiny <magpinyb@gmail.com>
2021-12-18 22:37:13 +0300

    production commits

--- studentcenter/settings.py
+++ studentcenter/settings.py
@@ -199,5 +199,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # prod_db = dj_database_url.config(conn_max_age=None)
 # DATABASES['default'].update(prod_db)
 
-# django_heroku.settings(locals())
-django_heroku.settings(locals(), staticfiles=False, logging=False)
+django_heroku.settings(locals())
+# django_heroku.settings(locals(), staticfiles=False, logging=False)
